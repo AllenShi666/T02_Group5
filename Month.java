6db81d7f79202b1dc36670ac2175b6826162cd73
Harrison Chen <hechen@ucalgary.ca>
2019-07-20 20:53:54 -0600

    Create Month.java
    Month Class

---
+++ Month.java
@@ -0,0 +1,21 @@
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Month {
+
+	private String monthName;
+	private int monthID;
+	private ArrayList<Day> monthDay = new ArrayList<Day>();
+	
+	
+	public Month(){
+		
+		
+	}
+	
+	public Month(String Calendar) {
+		
+		
+	}
+	
+}
