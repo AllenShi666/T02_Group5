50dbc559cc1955cb70f85e8b62a27b7e4b79fac0
Allen <AllenShi@Allens-MacBook-Pro.local>
2019-07-20 21:26:34 -0600

    this is test

---
+++ Month.java
@@ -0,0 +1,33 @@
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Month {
+
+	private String month_name;
+	private int month_id;
+	private ArrayList<Day> months_day = new ArrayList<Day>();
+	
+	
+	public Month(){
+		
+		
+	}
+	
+	public Month(String Calendar) {
+		
+		
+	}
+	
+	public void addDay(Day inputDay) {
+		
+		Day tempDay = inputDay;
+		
+		months_day.add(new Day(tempDay));
+	}
+	
+	public void displayMonth(ArrayList<Day> monthDays) {
+		
+		
+	}
+	
+}
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # T02_Group5
 Term Project
 //This is created on July 15, 2019
+// this is Vahbiz test
+
+//this is test
