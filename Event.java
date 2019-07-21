0c7859fe2ecb6e44f4fafd017b9441f2c6e946ea
AllenShi666 <52549261+AllenShi666@users.noreply.github.com>
2019-07-20 21:21:36 -0600

    Create Event

---
+++ Event.java
@@ -0,0 +1,26 @@
+//import java.util.Calendar;
+import java.util.*;
+
+public class Event {
+	private Calendar event_date;
+	private int id = 0;
+	private char privacy_type ='\0';
+	private String event_name = "";
+	private String event_type = "";
+	private String event_description = "";
+	private int color_id = 0;
+
+	public Event() {}
+	
+	public Event newEvent(Event newevent) {
+		return newevent.newEvent(newevent);
+	}
+	
+	public void displayEvent(ArrayList disp) {
+		
+	}
+	
+	
+	
+	
+}
