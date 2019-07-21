f90ad58722254d432435cb83afcc123dc36881e2
Harrison Chen <hechen@ucalgary.ca>
2019-07-20 21:04:57 -0600

    Update Month.java

--- Month.java
+++ Month.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 public class Month {
 
-	private String monthName;
-	private int monthID;
-	private ArrayList<Day> monthDay = new ArrayList<Day>();
+	private String month_name;
+	private int month_id;
+	private ArrayList<Day> months_day = new ArrayList<Day>();
 	
 	
 	public Month(){
@@ -14,6 +14,18 @@ public class Month {
 	}
 	
 	public Month(String Calendar) {
+		
+		
+	}
+	
+	public void addDay(Day inputDay) {
+		
+		Day tempDay = inputDay;
+		
+		months_day.add(new Day(tempDay));
+	}
+	
+	public void displayMonth(ArrayList<Day> monthDays) {
 		
 		
 	}
